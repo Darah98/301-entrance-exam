399df20b80f6a60199aa787f9a3b6b78f59874cf
darah98 <darashalabi@gmail.com>
2020-03-16 13:15:27 +0200

    q1solution

---
+++ js/q2.js
@@ -0,0 +1,15 @@
+"use strict";
+var starArrFinal = [];
+var starArr=['*'];
+var starList;
+function printStarPattern() {
+  for (var s = 0; s < 5; s++) {
+    for (var i = 0; i < starArr.length; i++) {
+    starArrFinal.push(starArr[i]);
+    }
+    console.log(starArrFinal);
+}
+// console.log(starArrFinal); 
+
+}
+printStarPattern();
